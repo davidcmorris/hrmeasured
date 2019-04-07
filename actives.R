cd0943ace2fb22a9055b74e48990abdefbf6ceb2
David C. Morris <davidcmorris@gmail.com>
2019-04-07 08:15:54 -0700

    Delete actives.R

--- actives.R
+++
@@ -1,7 +0,0 @@
-# Install tidyverse
-install.packages("tidyverse")
-# Load library(tidyverse)
-# Import data set (make sure to set working directory)
-staffing <- read_csv("20190406_hr_measured_staffing.csv")
-# Check to see if it read in correctly
-head(staffing)
